352d5964a05671a9cb7f5a770fb7c91fc30bf162
dsong17 <116125636+dsong17@users.noreply.github.com>
2024-04-24 16:22:10 -0500

    Add files via upload

---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Timer timer;
+    public Text scoreText;
+    public Text cumulativeScoreText;
+    private int score = 0;
+    private int cumulativeScore = 0; // Added variable to track cumulative score
+
+    void Start()
+    {
+        UpdateScoreUI();
+    }
+
+    private void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        cumulativeScoreText.text = "Total Score: " + GetCumulativeScore().ToString();
+    }
+
+    public void UpdateScore(int value)
+    {
+        score += value;
+        cumulativeScore += value; // Update cumulative score
+        UpdateScoreUI();
+        score = 0;
+    }
+
+    public float GetScore()
+    {
+        return score;
+    }
+
+    public float GetCumulativeScore()
+    {
+        return cumulativeScore; // Return cumulative score
+    }
+
+     public void CalculateScore()
+    {
+        float elapsedTime = timer.GetElapsedTime(); // Get the elapsed time from the timer
+        float tempscore = (2000/elapsedTime);
+        int tpscore = (int)tempscore;// Adjust the scoring mechanism as per your requirement
+        UpdateScore(tpscore);
+    }
+}
+
