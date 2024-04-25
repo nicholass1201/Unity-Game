31fc469a1b6ae0c4dc3c160a5cfca710bd4b510e
Winifred Ojo <116048868+Wyne1706@users.noreply.github.com>
2024-04-25 00:14:28 -0500

    Add files via upload

--- MainMenu/Scripts/SC_MainMenu.cs
+++ MainMenu/Scripts/SC_MainMenu.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class SC_MainMenu : MonoBehaviour
 {
+    public GameObject MainMenu;
     public void PlayNowButton()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("SampleScene");
     }
 
     public void QuitButton()
@@ -17,4 +18,13 @@ public class SC_MainMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("Player has quit the game!");
     }
+    // Function to toggle visibility of the main menu
+    public void ToggleMainMenu(bool show)
+    {
+        MainMenu.SetActive(show);
+        // Lock or unlock cursor based on menu visibility
+        Cursor.lockState = show ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = show;
+    }
 }
+
